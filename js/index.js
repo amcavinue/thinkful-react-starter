d5425a8dc48f8b233988e5e24cc8a9c16c2bf7d7
Aaron <aaronmcavinue@gmail.com>
2016-12-16 02:58:48 +0000

    Add module files for Trello board.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 require('babel-polyfill');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
+var Person = require('./components/person');
+var PersonList = require('./components/person-list.js')
+
+document.addEventListener('DOMContentLoaded', function() {
+    // ReactDOM.render(<Person />, document.getElementById('app'));
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
+});
